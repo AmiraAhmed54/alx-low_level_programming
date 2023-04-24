a26a6bf0ffbac30527ab8531c0aaade199ef3229
Amira <amiraahmed54@gmail.com>
2023-04-24 16:08:46 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stido.n> 
+/**
+ * main - A Program that prints a line with puts function
+ * Return: 0 (success) 
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
